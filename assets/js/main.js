b302c9e77ed20b8764bc25226abd519d0dfc5cab
DarkyVS <91894875+ArsalanAhmedSyed@users.noreply.github.com>
2023-12-05 21:43:19 +0000

    Created Project interaction
    + popup now appears which displays the project information
    
    + gab between the project image has been created
    
    HOLLY COW I MADE IT WORK HOOW???

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,6 +44,8 @@
 
 			});
 
+
+
 	// Sidebar.
 		if ($sidebar.length > 0) {
 
@@ -188,3 +190,101 @@
 			});
 
 })(jQuery);
+
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current control button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+//POP UP //
+//////////////////////////////////////////
+// Project Eve
+var modal = document.getElementById("ProjectEveModal");
+// Get the button that opens the modal
+var btn = document.getElementById("ProjectEveBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementById("ProjectEveClose");
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+
+//Two_Souls
+var TwoSoulsModal = document.getElementById("TwoSoulsModal");
+// Get the button that opens the modal
+var TwoSoulsBtn = document.getElementById("TwoSoulsBtn");
+
+// Get the <span> element that closes the modal
+var TwoSoulsClose = document.getElementById("TwoSoulsClose");
+
+// When the user clicks on the button, open the modal
+TwoSoulsBtn.onclick = function() {
+  TwoSoulsModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+TwoSoulsClose.onclick = function() {
+  TwoSoulsModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+  else if(event.target == TwoSoulsModal) {
+    TwoSoulsModal.style.display = "none";
+  }
+}
